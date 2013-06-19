f36cbb1981b7560a57e9ff853579c700c09d7060
MarkTaylor <m.b.taylor@bristol.ac.uk>
2013-06-19 14:52:46 +0100

    Document CdfFormatException

--- CdfFormatException.java
+++ CdfFormatException.java
@@ -2,14 +2,33 @@ package cdf;
 
 import java.io.IOException;
 
+/**
+ * Exception thrown during CDF parsing when the data stream appears either
+ * to be in contravention of the CDF format, or uses some feature of
+ * the CDF format which is unsupported by the current implementation.
+ *
+ * @author   Mark Taylor
+ * @since    18 Jun 2013
+ */
 public class CdfFormatException extends IOException {
 
+    /**
+     * Constructs an exception with a message.
+     *
+     * @param  msg  message
+     */
     public CdfFormatException( String msg ) {
         super( msg );
     }
 
-    public CdfFormatException( String msg, Throwable e ) {
+    /**
+     * Constructs an exception with a message and a cause.
+     *
+     * @param  msg  message
+     * @param  cause   upstream exception
+     */
+    public CdfFormatException( String msg, Throwable cause ) {
         super( msg );
-        initCause( e );
+        initCause( cause );
     }
 }
